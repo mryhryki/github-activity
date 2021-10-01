7df4adb560066f5ad9c6591fe1c0e5a055c45988
mryhryki <mryhryki@gmail.com>
2021-10-01 12:31:43 +0900

    Collect Issue comments

--- src/github/structs.rs
+++ src/github/structs.rs
@@ -3,23 +3,6 @@ use serde::Deserialize;
 use std::fmt;
 
 #[derive(Clone, Debug)]
-pub struct Owner {
-    pub login: String,
-    pub owner_type: OwnerType,
-}
-
-impl fmt::Display for Owner {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{login} ({owner_type})",
-            login = self.login,
-            owner_type = self.owner_type
-        )
-    }
-}
-
-#[derive(Clone, Debug)]
 pub enum OwnerType {
     User,
     Organization,
@@ -35,26 +18,14 @@ impl fmt::Display for OwnerType {
 }
 
 #[derive(Deserialize, Clone, Debug)]
-pub struct Repository {
-    pub id: i32,
-    pub name: String,
-    pub owner: OwnerForRepo,
-}
-
-impl fmt::Display for Repository {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{owner}/{name}",
-            owner = self.owner.login,
-            name = self.name
-        )
-    }
+pub struct RepoLogin {
+    pub login: String,
 }
 
 #[derive(Deserialize, Clone, Debug)]
-pub struct OwnerForRepo {
-    pub login: String,
+pub struct GitHubRepository {
+    pub name: String,
+    pub owner: RepoLogin,
 }
 
 #[derive(Deserialize, Clone, Debug)]
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+use crate::activity::structs::Activity;
 use crate::github::github_issue::get_issue_activity;
+use crate::github::github_issue_comment::get_issue_comments;
 
+mod activity;
 mod github;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let issue_activity = get_issue_activity().await?;
-    println!("{:#?}", issue_activity);
+    let mut activities: Vec<Activity> = vec![];
 
+    for issue in get_issue_activity().await? {
+        activities.push(Activity {
+            id: format!("issue:{}", issue.number),
+            title: issue.title,
+            url: issue.url,
+            datetime: issue.updatedAt,
+        });
+    }
+
+    for comment in get_issue_comments().await? {
+        activities.push(Activity {
+            id: format!("issue:{}:comment", comment.issue.number),
+            title: format!("Commented: {}", comment.bodyText),
+            url: comment.url,
+            datetime: comment.createdAt,
+        });
+    }
+
+    println!("{:#?}", activities);
     Ok(())
 }
