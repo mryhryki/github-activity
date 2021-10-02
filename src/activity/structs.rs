09ddcafd71f0dcec6a790dbfc615329bb1acb873
mryhryki <mryhryki@gmail.com>
2021-10-02 15:20:02 +0900

    Output markdown to Stdout

--- src/activity/structs.rs
+++ src/activity/structs.rs
@@ -1,6 +1,5 @@
 #[derive(Clone, Debug)]
 pub struct Activity {
-    pub id: String,
     pub repo: String, // e.g. "ORG/REPO"
     pub title: String,
     pub url: String,
---
+++ src/activity/writer.rs
@@ -0,0 +1,23 @@
+use std::cmp::Ordering;
+use crate::activity::structs::Activity;
+
+pub fn write_activities(mut activities: Vec<Activity>) {
+    activities.sort_by(|a1, a2| {
+        let a1id = format!("{}:{}", a1.repo, a1.title);
+        let a2id = format!("{}:{}", a2.repo, a2.title);
+        if a1id > a2id {
+            Ordering::Greater
+        } else {
+            Ordering::Less
+        }
+    });
+
+    let mut repo = String::from("");
+    for activity in activities {
+        if repo != activity.repo {
+            repo = String::from(&activity.repo);
+            println!("\n# {}\n", repo);
+        }
+        println!("- [{}]({})", activity.title.replace("\n", " "), activity.url)
+    }
+}
